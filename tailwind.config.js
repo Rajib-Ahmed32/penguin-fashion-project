3a0877bc217b26ab2fd56adf0ea50a6832e3283a
Rajib Ahmed <rajibahmed5434@gmail.com>
2025-01-26 19:32:34 +0600

    hero section revision, and cards section are created

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+            jost: ['Jost', 'sans-serif'], // Add 'Poppins' font
+        },
+      },
+    },
+    plugins: [],
+  };
+  
